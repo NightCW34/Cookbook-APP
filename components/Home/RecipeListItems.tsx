df1ba5c90c97c94b5338728a4690338653ab113a
Arlen Asael <asaelmarencojaime5867@gmail.com>
2022-10-23 18:21:56 -0600

    Add video items

--- components/Home/RecipeListItems.tsx
+++ components/Home/RecipeListItems.tsx
@@ -4,36 +4,59 @@ import {
   Text,
   View,
   SectionList,
-  Image,
   FlatList,
   TouchableOpacity,
+  ImageBackground,
 } from "react-native";
 
-import { Ionicons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import RecipeSection from "./RecipeSection";
+import { flingGestureHandlerProps } from "react-native-gesture-handler/lib/typescript/handlers/FlingGestureHandler";
 
 const ListItem = ({ item }: any) => {
   return (
     <View style={styles.item}>
       <View>
-        <Image
+        <ImageBackground
           source={{
             uri: item.uri,
           }}
           style={styles.itemPhoto}
+          imageStyle={{ borderRadius: 10 }}
           resizeMode="cover"
-        />
-        <TouchableOpacity style={styles.playbutton}>
-          <Ionicons
-            name="play"
-            size={25}
-            color="white"
-            style={{ opacity: 1 }}
-          />
-        </TouchableOpacity>
+        >
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <View style={styles.rating}>
+              <AntDesign name="star" size={15} color="white" />
+              <Text style={styles.rate_text}>{item.rating}</Text>
+            </View>
+            <View style={styles.bookmark}>
+              <View style={styles.fav}>
+                <Feather name="bookmark" size={25} color="black" />
+              </View>
+            </View>
+          </View>
+          <View style={styles.play_button}>
+            <TouchableOpacity style={styles.play}>
+              <Entypo name="controller-play" size={40} color="white" />
+            </TouchableOpacity>
+          </View>
+          <View style={{ width: "100%", alignItems: "flex-end" }}>
+            <View style={styles.time}>
+              <Text style={styles.rate_text}>{item.time}</Text>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
-      <View style={{ flexDirection: "row", marginTop: -50 }}>
+      <View style={{ flexDirection: "row" }}>
         <View>
           <Text style={styles.itemText}>{item.text}</Text>
         </View>
@@ -95,27 +118,37 @@ const SECTIONS = [
         key: "1",
         text: "How to make sushi at home",
         uri: "https://www.lavanguardia.com/files/og_thumbnail/uploads/2019/10/15/5e9977d4903ac.jpeg",
+        rating: "4,5",
+        time: "17:24",
       },
       {
         key: "2",
         text: "How to make hamburger at home",
         uri: "https://parade.com/.image/t_share/MTkwNTc4MzQwODAyNjAyMTA5/hamburger-with-fries-and-vegetables.jpg",
+        rating: "4,6",
+        time: "15:04",
       },
 
       {
         key: "3",
         text: "How to make a salad in home",
         uri: "https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549_960_720.jpg",
+        rating: "4,3",
+        time: "13:00",
       },
       {
         key: "4",
         text: "How to make a birthday cake",
         uri: "https://cdn.pixabay.com/photo/2015/08/31/23/42/cake-916253_960_720.jpg",
+        rating: "4,0",
+        time: "12:34",
       },
       {
         key: "5",
         text: "How to cook a tasty chicken",
         uri: "https://cdn.pixabay.com/photo/2016/02/24/03/04/chicken-1218968_960_720.jpg",
+        rating: "4,9",
+        time: "11:30",
       },
     ],
   },
@@ -138,16 +171,58 @@ const styles = StyleSheet.create({
     color: "black",
     fontFamily: "Poppins_600SemiBold",
   },
-  playbutton: {
-    zIndex: 1,
-    backgroundColor: "gray",
+  play: {
+    backgroundColor: "#B9B5B0",
+    borderRadius: 50,
     width: 60,
     height: 60,
-    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 4,
+    opacity: 0.8,
+  },
+  rating: {
+    backgroundColor: "#B9B5B0",
+    width: 60,
+    borderRadius: 5,
+    margin: 10,
+    opacity: 0.8,
     alignItems: "center",
     justifyContent: "center",
-    bottom: "50%",
-    left: "40%",
+    flexDirection: "row",
+  },
+  rate_text: {
+    color: "white",
+    fontFamily: "Poppins_600SemiBold",
+    paddingTop: 5,
+    paddingHorizontal: 5,
+  },
+  time: {
+    backgroundColor: "#B9B5B0",
+    width: 60,
+    borderRadius: 5,
+    margin: 10,
     opacity: 0.8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  play_button: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
+  },
+  bookmark: {
+    alignItems: "flex-end",
+    flex: 1,
+  },
+  fav: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
   },
 });
